f311dea29a1c8f223146280219e1739fb2c360bb
imSFD <93587850+imSFD@users.noreply.github.com>
2023-04-11 17:03:33 +0200

    Added some comments and started working on sending messages;

--- src/MyProtocol.java
+++ src/MyProtocol.java
@@ -5,11 +5,9 @@ import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.Random;
 
 /**
 * This is just some example code to show you how to interact 
@@ -24,7 +22,7 @@ public class MyProtocol{
     // The port to connect to. 8954 for the simulation server.
     private static int SERVER_PORT = 8954;
     // The frequency to use.
-    private static int frequency = 4801; //TODO: Set this to your group frequency!
+    private static int frequency = 4802; //TODO: Set this to your group frequency!
     // View the simulator at https://netsys.ewi.utwente.nl/integrationproject/
 
     private BlockingQueue<Message> receivedQueue;
@@ -48,6 +46,7 @@ public class MyProtocol{
 
     private String tempMsg = null; //Maybe rename?
 
+    //Function for sending a packet. Automatically detects if DATA or DATA_SHORT should be used
     private void sendMessage(String input) throws InterruptedException {
         byte[] input_b = input.getBytes();
         ByteBuffer toSend = ByteBuffer.allocate(input.length());
@@ -94,13 +93,6 @@ public class MyProtocol{
                     System.out.println("This node's index is: " + assignIndex());
 
                     sendMessage(myIndex+0);
-//                    byte[] index_b = (myIndex+0).getBytes(); //myIndex + distance to index (distance is 0 in this case)
-//                    ByteBuffer toSend = ByteBuffer.allocate(index_b.length); // copy data without newline / returns
-//                    toSend.put( index_b, 0, index_b.length); // enter data without newline / returns
-//                    Message msg;
-//
-//                    msg = new Message(MessageType.DATA_SHORT, toSend);
-//                    sendingQueue.put(msg);
 
                     continue;
                 }
@@ -117,22 +109,34 @@ public class MyProtocol{
                 if(command.equals("menu")){
                     continue; //TODO: implement
                 }
+                //Command syntax: send node "message"
+                //Example: send c "hello world"
                 if(command.startsWith("send")){
                     //TODO: packet sending
-                }
-//                read = System.in.read(temp.array()); // Get data from stdin, hit enter to send!
-//                if(read > 0){
-//                    if (temp.get(read-1) == '\n' || temp.get(read-1) == '\r' ) new_line_offset = 1; //Check if last char is a return or newline so we can strip it
-//                    if (read > 1 && (temp.get(read-2) == '\n' || temp.get(read-2) == '\r') ) new_line_offset = 2; //Check if second to last char is a return or newline so we can strip it
+                    String[] splitCommand = command.split(" ");
+                    //Packet header is 6 bytes total
+                    //(temporary) Packet header format: LENGTH=0 TTL=3 SEQ_NUM=0 SOURCEADDR DESTADDR PAYLOAD
+                    byte[] header = new byte[6];
+                    header[0] = 0; //length
+                    header[1] = 3; //ttl
+                    header[2] = 0; //Sequence number
+                    header[3] = 0; //Sequence number
+                    header[4] = myIndex.getBytes()[0]; //Source address
+                    header[5] = splitCommand[1].getBytes()[0]; //Destination addrress
+//                    byte length = 0;
+//                    byte ttl = 3;
+//                    byte[] seq_num = new byte[2];
+//                    byte sourceaddr = myIndex.getBytes()[0];
+//                    byte destaddr = splitCommand[1].getBytes()[0];
+                    byte[] payload = splitCommand[2].getBytes();
 
-////                    if( (read-new_line_offset) == )
-//                    if( (read-new_line_offset) > 2 ){
-//                        msg = new Message(MessageType.DATA, toSend);
-//                    } else {
-//                        msg = new Message(MessageType.DATA_SHORT, toSend);
-//                    }
-//                    sendingQueue.put(msg);
-//                }
+                    String packet = Base64.getEncoder().encodeToString(header) +
+                                    Base64.getEncoder().encodeToString(payload);
+
+                    System.out.println("Sent packet: " + packet);
+                    sendMessage(packet);
+                    continue;
+                }
             }
         } catch (InterruptedException e){
             System.exit(2);
@@ -171,32 +175,31 @@ public class MyProtocol{
                         System.out.println("BUSY");
                     } else if (m.getType() == MessageType.FREE){
                         System.out.println("FREE");
-                        if(tempMsg != null){
+                        if(tempMsg != null){ //Whenever the channel is free, check if we have a message to send.
                             sendMessage(tempMsg);
-//                            byte[] msg = tempMsg.getBytes(); //myIndex + distance to index (distance is 0 in this case)
-//                            ByteBuffer toSend = ByteBuffer.allocate(msg.length); // copy data without newline / returns
-//                            toSend.put( msg, 0, msg.length); // enter data without newline / returns
-//                            Message message;
-//
-//                            message = new Message(MessageType.DATA_SHORT, toSend);
-//                            sendingQueue.put(message);
-                            tempMsg = null;
+                            tempMsg = null; //After sending the message, reset the message buffer to null.
                         }
                     } else if (m.getType() == MessageType.DATA){
                         System.out.print("DATA: ");
-                        printByteBuffer( m.getData(), m.getData().capacity() ); //Just print the data
+//                        printByteBuffer( m.getData(), m.getData().capacity() ); //Just print the data
+//
+//                        byte[] test = m.getData().array();
+//                        byte[] payload = Arrays.copyOfRange(test, 6, test.length-1);
+//                        String s =  Base64.getEncoder().encodeToString(payload);; //Decode the data recieved
+                        String s = StandardCharsets.UTF_8.decode(m.getData()).toString(); //Decode the data recieved
+                        System.out.println(s); //Print the data
                     } else if (m.getType() == MessageType.DATA_SHORT){
                         System.out.print("DATA_SHORT: ");
                         String s = StandardCharsets.UTF_8.decode(m.getData()).toString(); //Decode the data recieved
                         System.out.println(s); //Print the data
                         if(distanceVector.containsKey(s.substring(0,1))){//If index is not this node's index, add the route to distanceVector
                             continue;
-                        } else {
+                        } else { //Also, retransmit the DATA_SHORT packet
                             String index = s.substring(0,1);
                             Integer distance = Integer.valueOf(s.substring(1,2));
                             possibleIndex.remove(index);
                             distanceVector.put(index, distance + 1);
-                            tempMsg = index + (distance+1);
+                            tempMsg = index + (distance+1); //Store the message in a temporary variable. This message will be sent when the channel is FREE
                         }
                     } else if (m.getType() == MessageType.DONE_SENDING){
                         System.out.println("DONE_SENDING");
