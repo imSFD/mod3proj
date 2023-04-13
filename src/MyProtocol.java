72513479ef09c2ab266375cd9f043b6325c65e83
Alex Toma <a.toma@student.uwente.nl>
2023-04-13 19:39:31 +0300

    aloha  \w Mihai

--- src/MyProtocol.java
+++ src/MyProtocol.java
@@ -176,6 +176,7 @@ public class MyProtocol{
         }
 
         public void run(){
+            int timeToWait = 1;
             while(true) {
                 try{
                     Message m = receivedQueue.take();
